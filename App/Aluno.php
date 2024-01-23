8d0a47c82e14ffd20523509d9269e6d37c6ea664
Cawe541 <cawe.wewe04@gmail.com>
2024-01-23 13:45:42 -0300

    first commit

---
+++ App/Aluno.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+class Aluno
+{
+    private string $cpf;
+    private string $nome;
+    private Email $email;
+}
---
+++ App/Email.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Stringable;
+
+class Email implements Stringable
+{
+    private string $endereco;
+
+    public function __construct(string $endereco)
+    {
+        if (filter_var($endereco, FILTER_VALIDATE_EMAIL) === false) {
+            throw new \InvalidArgumentException('Endereço de e-mail inválido.');
+        }
+        $this->endereco = $endereco;
+    }
+
+    public function __toString()
+    {
+        return $this->endereco;
+    }
+
+}
