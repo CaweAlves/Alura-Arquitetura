3373055299f2e6264b08d8c10c89ea6be46d99fa
Cawe541 <cawe.wewe04@gmail.com>
2024-01-30 20:51:28 -0300

    feat(student): add phone attribute

--- App/Aluno.php
+++ App/Aluno.php
@@ -4,7 +4,14 @@ namespace App;
 
 class Aluno
 {
-    private string $cpf;
+    private Cpf $cpf;
     private string $nome;
     private Email $email;
+    private array $telefone;
+
+    public function adicionarTelefone(string $ddd, string $numero)
+    {
+        $this->telefone[] = new Telefone($ddd, $numero);
+    }
+
 }
