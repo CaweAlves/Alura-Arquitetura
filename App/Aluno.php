7b109be9849e7a3312460246cf0f9c102b65b97e
CaweAlves <cawe.wewe04@gmail.com>
2024-01-31 14:05:04 -0300

    feat(student): add construct method

--- App/Aluno.php
+++ App/Aluno.php
@@ -9,6 +9,13 @@ class Aluno
     private Email $email;
     private array $telefone;
 
+    public function __construct(Cpf $cpf, string $nome, Email $email)
+    {
+        $this->cpf = $cpf;
+        $this->nome = $nome;
+        $this->email = $email;
+    }
+
     public function adicionarTelefone(string $ddd, string $numero)
     {
         $this->telefone[] = new Telefone($ddd, $numero);
